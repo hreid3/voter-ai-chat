f47791741185dec93b975faf4111b1ec03b3528b
dukesvrtech <horace.reid@bluenetreflections.com>
2024-12-03 09:42:28 -0500

    Read markdown files from file system

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,26 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    ppr: false,
-  },
-  images: {
-    remotePatterns: [
-      {
-        hostname: 'avatar.vercel.sh',
-      },
-    ],
-  },
+	/* config options here */
+	experimental: {
+		ppr: false,
+	},
+	images: {
+		remotePatterns: [
+			{
+				hostname: 'avatar.vercel.sh',
+			},
+		],
+	},
+	webpack: (config) => {
+		// Add support for importing .md files as strings
+		config.module.rules.push({
+			test: /\.md$/,
+			use: 'raw-loader',
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
