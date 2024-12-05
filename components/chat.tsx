15a528b6e542cf41b8f1a7121c7de4f8feed7f99
dukesvrtech <horace.reid@bluenetreflections.com>
2024-12-05 14:00:00 -0500

    Improved error handling and refactored deprecated streaming class

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,173 +1,180 @@
 'use client';
 
-import type {Attachment, Message} from 'ai';
-import {useChat} from 'ai/react';
-import {AnimatePresence} from 'framer-motion';
-import {useState} from 'react';
-import useSWR, {useSWRConfig} from 'swr';
-import {useWindowSize} from 'usehooks-ts';
+import type { Attachment, Message } from 'ai';
+import { useChat } from 'ai/react';
+import { AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
+import { useWindowSize } from 'usehooks-ts';
 
-import {ChatHeader} from '@/components/chat-header';
-import {PreviewMessage, ThinkingMessage} from '@/components/message';
-import {useScrollToBottom} from '@/components/use-scroll-to-bottom';
-import type {Vote} from '@/lib/db/schema';
-import {fetcher} from '@/lib/utils';
+import { ChatHeader } from '@/components/chat-header';
+import { PreviewMessage, ThinkingMessage } from '@/components/message';
+import { useScrollToBottom } from '@/components/use-scroll-to-bottom';
+import type { Vote } from '@/lib/db/schema';
+import { fetcher } from '@/lib/utils';
 
-import {Block, type UIBlock} from './block';
-import {BlockStreamHandler} from './block-stream-handler';
-import {MultimodalInput} from './multimodal-input';
-import {Overview} from './overview';
+import { Block, type UIBlock } from './block';
+import { BlockStreamHandler } from './block-stream-handler';
+import { MultimodalInput } from './multimodal-input';
+import { Overview } from './overview';
+import { toast } from 'sonner';
 
 export const hideToolUiList = [
-  "fetchTableDdls",
-  "executeSelects",
+	"fetchTableDdls",
+	"executeSelects",
 	"listVoterDataMappingKeysTool",
 	"voterDataColumnLookupTool",
 	"fetchStaticMapTool",
 ];
 
 export function Chat({
-  id,
-  initialMessages,
-  selectedModelId,
-}: {
-  id: string;
-  initialMessages: Array<Message>;
-  selectedModelId: string;
+											 id,
+											 initialMessages,
+											 selectedModelId,
+										 }: {
+	id: string;
+	initialMessages: Array<Message>;
+	selectedModelId: string;
 }) {
-  const { mutate } = useSWRConfig();
-	const [streaming,setStreaming] = useState(false);
-  const {
-    messages: originalMessages,
-    setMessages,
-    handleSubmit,
-    input,
-    setInput,
-    append,
-    isLoading,
-    stop,
-    data: streamingData,
+	const {mutate} = useSWRConfig();
+	const [streaming, setStreaming] = useState(false);
+	const {
+		messages: originalMessages,
+		setMessages,
+		handleSubmit,
+		input,
+		setInput,
+		append,
+		isLoading,
+		stop,
+		data: streamingData,
 		error,
-  } = useChat({
-    body: { id, modelId: selectedModelId },
-    initialMessages,
+	} = useChat({
+		body: {id, modelId: selectedModelId},
+		initialMessages,
 		onResponse: () => {
 			setStreaming(true);
 		},
-    onFinish: () => {
-      mutate('/api/history');
+		onFinish: () => {
+			mutate('/api/history');
 			setStreaming(false)
-    },
-  });
-  const { width: windowWidth = 1920, height: windowHeight = 1080 } =
-    useWindowSize();
+		},
+	});
+	const {width: windowWidth = 1920, height: windowHeight = 1080} =
+		useWindowSize();
 
-  const [block, setBlock] = useState<UIBlock>({
-    documentId: 'init',
-    content: '',
-    title: '',
-    status: 'idle',
-    isVisible: false,
-    boundingBox: {
-      top: windowHeight / 4,
-      left: windowWidth / 4,
-      width: 250,
-      height: 50,
-    },
-  });
+	const [block, setBlock] = useState<UIBlock>({
+		documentId: 'init',
+		content: '',
+		title: '',
+		status: 'idle',
+		isVisible: false,
+		boundingBox: {
+			top: windowHeight / 4,
+			left: windowWidth / 4,
+			width: 250,
+			height: 50,
+		},
+	});
 
-  const { data: votes } = useSWR<Array<Vote>>(
-    `/api/vote?chatId=${id}`,
-    fetcher,
-  );
+	const {data: votes} = useSWR<Array<Vote>>(
+		`/api/vote?chatId=${id}`,
+		fetcher,
+	);
 
-  const [messagesContainerRef, messagesEndRef] =
-    useScrollToBottom<HTMLDivElement>();
+	const [messagesContainerRef, messagesEndRef] =
+		useScrollToBottom<HTMLDivElement>();
 
-  const [attachments, setAttachments] = useState<Array<Attachment>>([]);
+	const [attachments, setAttachments] = useState<Array<Attachment>>([]);
 	const messages = originalMessages
 		.filter(v => !(v.toolInvocations
 			?.find(u => hideToolUiList
 				?.some(t => t === u.toolName))))
 
-  return (
-    <>
-      <div className="flex flex-col min-w-0 h-dvh bg-background">
-        <ChatHeader selectedModelId={selectedModelId} />
-        <div
-          ref={messagesContainerRef}
-          className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
-        >
-          {messages.length === 0 && <Overview />}
+	useEffect(() => {
+		if (error) {
+			console.error(error);
+			toast.error(`Processing error: ${error.message}`);
+		}
+	}, [error])
+	return (
+		<>
+			<div className="flex flex-col min-w-0 h-dvh bg-background">
+				<ChatHeader selectedModelId={selectedModelId}/>
+				<div
+					ref={messagesContainerRef}
+					className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
+				>
+					{messages.length === 0 && <Overview/>}
 
-          {messages.map((message, index) => (
-            <PreviewMessage
-              key={message.id}
-              chatId={id}
-              message={message}
-              block={block}
-              setBlock={setBlock}
-              isLoading={isLoading && messages.length - 1 === index}
+					{messages.map((message, index) => (
+						<PreviewMessage
+							key={message.id}
+							chatId={id}
+							message={message}
+							block={block}
+							setBlock={setBlock}
+							isLoading={isLoading && messages.length - 1 === index}
 							streaming={streaming}
-              vote={
-                votes
-                  ? votes.find((vote) => vote.messageId === message.id)
-                  : undefined
-              }
-            />
-          ))}
+							vote={
+								votes
+									? votes.find((vote) => vote.messageId === message.id)
+									: undefined
+							}
+						/>
+					))}
 
-          {isLoading &&
-            messages.length > 0 &&
-            messages[messages.length - 1].role === 'user' && (
-              <ThinkingMessage />
-            )}
+					{isLoading &&
+						messages.length > 0 &&
+						messages[messages.length - 1].role === 'user' && (
+							<ThinkingMessage/>
+						)}
 
-          <div
-            ref={messagesEndRef}
-            className="shrink-0 min-w-[24px] min-h-[24px]"
-          />
-        </div>
-        <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
-          <MultimodalInput
-            chatId={id}
-            input={input}
-            setInput={setInput}
-            handleSubmit={handleSubmit}
-            isLoading={isLoading}
-            stop={stop}
-            attachments={attachments}
-            setAttachments={setAttachments}
-            messages={messages}
-            setMessages={setMessages}
-            append={append}
-          />
-        </form>
-      </div>
+					<div
+						ref={messagesEndRef}
+						className="shrink-0 min-w-[24px] min-h-[24px]"
+					/>
+				</div>
+				<form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
+					<MultimodalInput
+						chatId={id}
+						input={input}
+						setInput={setInput}
+						handleSubmit={handleSubmit}
+						isLoading={isLoading}
+						stop={stop}
+						attachments={attachments}
+						setAttachments={setAttachments}
+						messages={messages}
+						setMessages={setMessages}
+						append={append}
+					/>
+				</form>
+			</div>
 
-      <AnimatePresence>
-        {block?.isVisible && (
-          <Block
-            chatId={id}
-            input={input}
-            setInput={setInput}
-            handleSubmit={handleSubmit}
-            isLoading={isLoading}
-            stop={stop}
-            attachments={attachments}
-            setAttachments={setAttachments}
-            append={append}
-            block={block}
-            setBlock={setBlock}
-            messages={messages}
-            setMessages={setMessages}
-            votes={votes}
+			<AnimatePresence>
+				{block?.isVisible && (
+					<Block
+						chatId={id}
+						input={input}
+						setInput={setInput}
+						handleSubmit={handleSubmit}
+						isLoading={isLoading}
+						stop={stop}
+						attachments={attachments}
+						setAttachments={setAttachments}
+						append={append}
+						block={block}
+						setBlock={setBlock}
+						messages={messages}
+						setMessages={setMessages}
+						votes={votes}
 						streaming={streaming}
-          />
-        )}
-      </AnimatePresence>
+					/>
+				)}
+			</AnimatePresence>
 
-      <BlockStreamHandler streamingData={streamingData} setBlock={setBlock} />
-    </>
-  );
+			<BlockStreamHandler streamingData={streamingData} setBlock={setBlock}/>
+		</>
+	);
 }
