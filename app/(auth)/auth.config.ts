bd5d04291c6424e79ab3a4e408f743eaeb8c2982
dukesvrtech <horace.reid@bluenetreflections.com>
2024-12-05 14:45:59 -0500

    removed trusted

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -9,7 +9,6 @@ export const authConfig = {
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
   ],
-	trustHost: false,
 	debug: true,
 
   callbacks: {
