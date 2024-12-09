d09583c8bb3fa0595114d4f821f2d7cdb1047b4b
dukesvrtech <horace.reid@bluenetreflections.com>
2024-12-08 22:16:26 -0500

    Added rippled effect to the UI.

--- components/message.tsx
+++ components/message.tsx
@@ -13,6 +13,7 @@ import { Markdown } from './markdown';
 import { MessageActions } from './message-actions';
 import { PreviewAttachment } from './preview-attachment';
 import ErrorBubble from "@/components/ui/error-bubble";
+import RippleEffect from "@/components/RippleEffect";
 
 export const hideToolUiList = [
 	"fetchTableDdls",
@@ -56,7 +57,7 @@ export const PreviewMessage = ({
 			>
 				{message.role === 'assistant' && (
 					<div className="size-8 flex items-center rounded-full justify-center ring-1 shrink-0 ring-border">
-						<SparklesIcon size={14}/>
+						<RippleEffect isAnimating={streaming || isLoading}><SparklesIcon size={14}/></RippleEffect>
 					</div>
 				)}
 
@@ -115,38 +116,6 @@ export const PreviewMessage = ({
 						vote={vote}
 						isLoading={isLoading}
 					/>
-				</div>
-			</div>
-		</motion.div>
-	);
-};
-
-export const ThinkingMessage = () => {
-	const role = 'assistant';
-
-	return (
-		<motion.div
-			className="w-full mx-auto max-w-3xl px-4 group/message "
-			initial={{y: 5, opacity: 0}}
-			animate={{y: 0, opacity: 1, transition: {delay: 1}}}
-			data-role={role}
-		>
-			<div
-				className={cx(
-					'flex gap-4 group-data-[role=user]/message:px-3 w-full group-data-[role=user]/message:w-fit group-data-[role=user]/message:ml-auto group-data-[role=user]/message:max-w-2xl group-data-[role=user]/message:py-2 rounded-xl',
-					{
-						'group-data-[role=user]/message:bg-muted': true,
-					},
-				)}
-			>
-				<div className="size-8 flex items-center rounded-full justify-center ring-1 shrink-0 ring-border">
-					<SparklesIcon size={14}/>
-				</div>
-
-				<div className="flex flex-col gap-2 w-full">
-					<div className="flex flex-col gap-4 text-muted-foreground">
-						Thinking...
-					</div>
 				</div>
 			</div>
 		</motion.div>
