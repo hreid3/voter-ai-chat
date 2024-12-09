85f1490051e03d8424b04b8a6ea1c286c001355f
dukesvrtech <horace.reid@bluenetreflections.com>
2024-12-08 19:00:54 -0500

    Initial commit

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -112,7 +112,7 @@ export async function POST(request: Request) {
 						streamingData.writeData('call completed');
 					},
 					experimental_telemetry: {
-						isTrackingEnabledisTrackingEnabled: true,
+						isEnabled: true,
 						functionId: 'stream-text',
 					},
 				});
